f1df3815a0fd801ea910b2bb90841a1ed11973dc
paultreanor <treanorpaul9@gmail.com>
2023-08-02 15:00:16 +0100

    Install TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,html}',
+    './index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
