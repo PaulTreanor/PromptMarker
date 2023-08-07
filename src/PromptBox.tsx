8c9527232e49a2bd33362e0ac0aa0e0f30e5148c
paultreanor <treanorpaul9@gmail.com>
2023-08-07 16:32:35 +0100

    Remove needless href from div element

--- src/PromptBox.tsx
+++ src/PromptBox.tsx
@@ -35,7 +35,7 @@ export default function PromptBox ({ InsertPrompt, PromptData }: PromptBoxProps)
 
   return (
         <div onDoubleClick={handleDoubleClick} onClick={handleClick} className="mt-3 bg-slate-50 hover:bg-slate-100">
-            <div href="#" className="block max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div className="block max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <h5 className="mb-1 font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                 <p className="font-normal text-gray-700 dark:text-gray-400 overflow-hidden overflow-ellipsis max-h-[3em] whitespace-nowrap">{contents}</p>
             </div>
