a19ff2242cb03e60a99b84dab1548d0e1dcc212b
paultreanor <treanorpaul9@gmail.com>
2023-08-25 14:54:36 +0100

    Update default instructions

--- electron/default-prompts.ts
+++ electron/default-prompts.ts
@@ -1,20 +1,20 @@
 const initialPrompts = [
   {
     id: 'f9a52f87-a234-4860-a0cf-27e6f16b297c',
-    title: 'JavaScript dev',
-    text: 'I am an expert JavaScript developer. I want you to answer my questions as an expert JavaScript developer.',
+    title: 'AI Assistant',
+    text: "Give me short and concise answers. Don't tell me that you are an AI language model. You don't have to explain every line of code that you write, I am a profesional JS developer, I know what I'm doing.",
     isNew: false
   },
   {
     id: 'f9a52f87-a234-4860-a0cf-27e6f16b297d',
-    title: 'Prompt 2',
-    text: 'This is the second prompt',
+    title: 'English 2nd language',
+    text: 'English isn’t my first language, so please answer in simple English and avoid using idioms or complex jargon.',
     isNew: false
   },
   {
     id: 'f9a52f87-a234-4860-a0cf-27e6f16b297e',
-    title: 'Code Review',
-    text: 'This is the third prompt',
+    title: 'JS Code Review',
+    text: 'I am a professional JavaScript developer seeking constructive feedback on my code. Please provide detailed suggestions for improvements, point out potential issues, and highlight best practices. Consider performance, readability, and maintainability in your review.',
     isNew: false
   }
 ]
